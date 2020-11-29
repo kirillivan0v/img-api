624654f14fb992a9e1ed6e0ac3ee43d674f7d55f
Kirill I <54040921+kirillivan0v@users.noreply.github.com>
2020-11-29 23:50:33 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,26 +2,60 @@ window.onload = function(){
     const API_KEY = '19316226-1eddace69c97e7c82ba53a287';
 
     function httpRequest(query){
-        let resultArray = [];
-        let request = new XMLHttpRequest();
-            request.open('GET', `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo`);
-            request.send();
 
-            setTimeout(() => {
+        return new Promise(function(success, fail){
+            let request = new XMLHttpRequest();
+                request.open('GET', `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo`);
+        
+            request.addEventListener("load", function() {
+                if (request.status < 400)
+                    success(request.response);
+                else
+                    fail(new Error("Request failed: " + request.statusText));
+            });
+                 
+                  request.send();
+        })
+
+
+            /* setTimeout(() => {
                 let res = JSON.parse(request.responseText);
                 res.hits.forEach(element => {
                     resultArray.push(element.largeImageURL);
                 });
-                
-            }, 1000);
+                console.log(resultArray[2]);
+                return resultArray;
+            }, 1000).then(function(){alert()}) */
             
-            console.log(resultArray);
-            return resultArray; 
+            
+            // return resultArray; 
     }
 
-    let mass = httpRequest('cats');
-
     
-    console.log(mass)
+
+    httpRequest('cats')
+    .then(text => {
+        // let massive = [];
+        JSON.parse(text).hits.forEach(element => {
+            // massive.push(element.largeImageURL);
+            let div = document.createElement('div');
+                div.classList.add('carousel-item');
+            
+            let img = document.createElement('img');
+                img.classList.add('d-block');
+                img.classList.add('w-100');
+
+                img.src = element.largeImageURL;
+
+            div.appendChild(img);
+            document.querySelector('.carousel-inner').appendChild(div);
+        });
+
+        document.querySelector('.carousel-inner').firstElementChild.classList.add('active');
+
+        
+    })
+    .catch(err => console.log(err));
+
 
 }
